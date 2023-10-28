f6e64887084d083ee40aa7246ccbbca003171a1f
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-10-28 14:22:27 +0600

    grasped the optimized way of using useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Clock from './components/Clock';
 import './App.css';
 import SideEffectWithClass from './components/SideEffectWithClass';
 import SideEffectWithFunction from './components/SideEffectWithFunction';
+import { useState } from 'react';
 
 function App() {
+	const [show, setShow] = useState(true);
 	return (
 		<div>
 			{/* <Clock locale={'bn-BD'} /> */}
 			{/* <SideEffectWithClass /> */}
-			<SideEffectWithFunction />
+			{show && <SideEffectWithFunction setShow={setShow} />}
 		</div>
 	);
 }
